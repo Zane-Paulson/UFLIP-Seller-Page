0cf1284b1277b76a0a0a516215f2990b8bb2035a
Zane-Paulson <zanepaulson@uflip.com>
2023-07-11 11:56:10 -0500

    Completed Website :D

--- js/script.js
+++ js/script.js
@@ -3,9 +3,15 @@
 const navMenuBtnOpen = document.querySelector(".nav-menu-btn");
 
 const headerMobile = document.querySelector(".header-mobile");
+
 const dropdown = document.querySelector(".dropdown-container");
+
+const navMenuBtnIcon = document.querySelectorAll(".nav-btn");
 
 navMenuBtnOpen.addEventListener("click", function () {
     dropdown.classList.toggle("active-dropdown");
+
+    navMenuBtnIcon.forEach((el) => el.classList.toggle("active-header-mobile"));
+
     headerMobile.classList.toggle("active-header-mobile");
 });
